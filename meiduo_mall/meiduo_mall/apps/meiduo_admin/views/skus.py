ff73bc10b6f0907c6b2984b3a9f6ad2034b0639f
clarence <2263194561@qq.com>
2020-08-01 11:23:13 +0800

    图片管理-获取sku简单信息-ListAPIView写

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/skus.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/skus.py
@@ -2,6 +2,7 @@ from goods.models import SKU
 from goods.models import SKUImage
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
 from meiduo_admin.serializers.skus import SKUImageSerializer
@@ -14,21 +15,11 @@ class SKUImageViewSet(ModelViewSet):
 
 
 # GET /meiduo_admin/skus/simple/
-class SKUSimpleView(APIView):
+class SKUSimpleView(ListAPIView):
+    queryset = SKU.objects.all()
     permission_classes = [IsAdminUser]
-
-    def get(self, request):
-        """
-        获取SKU简单数据：
-        ① 查询数据库获取所有SKU的数据
-        ② 将SKU数据序列化返回
-        """
-        # ① 查询数据库获取所有SKU的数据
-        skus = SKU.objects.all()
-
-        # ② 将SKU数据序列化返回
-        serializer = SKUSimpleSerializer(skus, many=True)
-        return Response(serializer.data)
+    # 关闭分页
+    pagination_class = None
 
 
 
